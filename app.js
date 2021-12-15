32f8173545f089bde1a7820f54ce83ce3a78c927
Thong Nguyen <nguyenthong17@gmail.com>
2021-12-15 16:26:08 +0100

    commit

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ app.use('/login', loginRoutes)
 const adminRoutes = require("./routes/admin");
 app.use("/admin", isAuthenticated, adminRoutes);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
